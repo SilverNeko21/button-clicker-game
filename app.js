7772f75eb974c0b0e6c4f988a2b0cfe9fb740705
SilverNeko21 <larong2122@gmail.com>
2018-08-20 11:26:50 -1000

    oof

--- app.js
+++ app.js
@@ -8,9 +8,13 @@ function increments(){
     score = score + 1;
     document.getElementById("Number").innerHTML = score;
     console.log(score);
-    //if(score >= 10) {
-       // document.getElementById("pop1").innerHTML = "<img id='sagiri2' src='https://data.whicdn.com/images/287156172/large.jpg'>";
-    //}
+    if(score == 10) {
+        document.getElementById("pop1").innerHTML = "<img class='sagiri2' src='https://data.whicdn.com/images/287156172/large.jpg'>";
+    }
+
+    if(score == 70) {
+        document.getElementById("pop2").innerHTML = "<img class='sagiri2' src='https://assets.entrepreneur.com/content/3x2/2000/20180703190744-rollsafe-meme.jpeg?width=700&crop=2:1'>";
+    }
    
     if(score == 50) {
         document.getElementById("musicArea").innerHTML = " <audio autoplay> <source src='Audio/Sun and Moon Rival Gladion Remix.mp3' type='audio/mpeg'> </audio>";
