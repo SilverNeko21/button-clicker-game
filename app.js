362c503aa3828730f3c9d8c639155bc38aa1be8e
SilverNeko21 <larong2122@gmail.com>
2018-08-19 19:46:11 -1000

    wow

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var p ;
+var p;
 var score = 0;
 
 document.getElementById("button").addEventListener("click", increments);
@@ -8,5 +8,7 @@ function increments(){
     score = score + 1;
     document.getElementById("Number").innerHTML = score;
     console.log(score);
+    //if(score >= 10) {
+        //document.getElementById("pop1").innerHTML = "<img id='sagiri2' src='https://data.whicdn.com/images/287156172/large.jpg'>";
+    //}
 }
- 
