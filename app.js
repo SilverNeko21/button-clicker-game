782d9289a14827ce682f76be1b5bbe4a275a2a36
SilverNeko21 <larong2122@gmail.com>
2018-08-19 21:14:08 -1000

    WE GOT MUSICgit add .git add .

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ function increments(){
     document.getElementById("Number").innerHTML = score;
     console.log(score);
     //if(score >= 10) {
-        //document.getElementById("pop1").innerHTML = "<img id='sagiri2' src='https://data.whicdn.com/images/287156172/large.jpg'>";
+       // document.getElementById("pop1").innerHTML = "<img id='sagiri2' src='https://data.whicdn.com/images/287156172/large.jpg'>";
     //}
+   
+    if(score == 50) {
+        document.getElementById("musicArea").innerHTML = " <audio autoplay> <source src='Audio/Sun and Moon Rival Gladion Remix.mp3' type='audio/mpeg'> </audio>";
+    }
 }
