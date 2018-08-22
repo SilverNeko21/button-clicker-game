7b558195553593a9fa31aeac4ffd319cf7c502c6
SilverNeko21 <larong2122@gmail.com>
2018-08-21 23:37:00 -1000

    Added some randomness

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var p;
+var p = ["<audio autoplay> <source src='Audio/Sun and Moon Rival Gladion Remix.mp3' type='audio/mpeg'> </audio>", "<audio autoplay> <source src='Audio/Blend W (320  kbps) (ytformp3.com).mp3' type='audio/mpeg'> </audio>", "<audio autoplay> <source src='Audio/オーバーロードIII OP  Overlord Season 3 Opening Full『MYTH  ROID - VORACITY』(ENG SUB).mp3'> </audio>", "<audio autoplay> <source src='Audio/How Not to Summon a Demon Lord - Ending (HD).mp3' type='audio/mpeg'> </audio>"];
 var score = 0;
 
 document.getElementById("button").addEventListener("click", increments);
@@ -13,10 +13,26 @@ function increments(){
     }
 
     if(score == 70) {
-        document.getElementById("pop2").innerHTML = "<img class='sagiri2' src='https://assets.entrepreneur.com/content/3x2/2000/20180703190744-rollsafe-meme.jpeg?width=700&crop=2:1'>";
+        document.getElementById("pop2").innerHTML = "<img class='sagiri2' src='http://iskin.tooliphone.net/themes/16097/10686/preview-256.png'>";
+    }
+
+    if(score == 130) {
+        document.getElementById("pop3").innerHTML = "<img class='sagiri2' src='https://t00.deviantart.net/lhDcBcQsMgxSnhCEsCpEBD1XoEY=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre00/3400/th/pre/f/2017/120/d/6/wip_2_by_fhilippe124-db7q4hs.png'>";
+    }
+
+    if(score == 200) {
+        document.getElementById("pop5").innerHTML = "<img class='sagiri2' src='https://pbs.twimg.com/media/C-yHgIkXgAE9MQb.jpg'>";
     }
    
     if(score == 50) {
-        document.getElementById("musicArea").innerHTML = " <audio autoplay> <source src='Audio/Sun and Moon Rival Gladion Remix.mp3' type='audio/mpeg'> </audio>";
+        i = Math.floor((Math.random() * p.length));
+        document.getElementById("musicArea").innerHTML = p[i];
     }
 }
+
+
+//document.getElementById("shop").addEventListener("click", openShop);
+
+//function openShop{
+
+//}
